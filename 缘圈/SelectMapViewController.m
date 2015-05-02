9f2e04e53b8015c381cd00b0c5db003e17b90cdf
SeanXuCn <271405081@qq.com>
2015-05-02 09:51:14 +0800

    添加完毕 高德地图的sdk

--- 缘圈/SelectMapViewController.m
+++ 缘圈/SelectMapViewController.m
@@ -11,25 +11,53 @@
 #import <CoreLocation/CoreLocation.h>
 #import "DXPopover.h"
 #import "HP_NavigationController.h"
+#import "MBProgressHUD+HP.h"
 
 
 @interface SelectMapViewController ()<UISearchBarDelegate,MKMapViewDelegate,CLLocationManagerDelegate,UITableViewDelegate,UITableViewDataSource>
 
 @property (nonatomic, strong, readwrite) IBOutlet UISearchBar       *searchBar;
-@property (nonatomic, strong, readwrite) CLLocationManager *locationMgr;
 @property (nonatomic, strong, readwrite) IBOutlet MKMapView         *mapView;
 @property (nonatomic, strong, readwrite) IBOutlet UITableView       *searchHistoryTableView;//历史记录tableView
 @property (nonatomic, strong, readwrite) IBOutlet UIView            *contentView;//容器View。包装searchBar和MapView实现动画上移效果
 @property (weak, nonatomic) IBOutlet UIView *buttomTabBar;
 @property (weak, nonatomic) IBOutlet UILabel *tabBarLabel;
+
+@property (nonatomic, strong, readwrite) NSMutableArray *alternativeAddressesM;	//备选目的地
+@property (nonatomic, strong, readwrite) NSMutableArray *historyAddressM;		//历史搜索地址
+@property (nonatomic, strong, readwrite) CLLocationManager *locationMgr;
+@property (nonatomic, strong) CLGeocoder *geocoder;
+@property (nonatomic, strong, readwrite) MKUserLocation *userLocation;
 - (IBAction)tabBarConfirmBtnClick:(UIButton *)sender;
-
-
 @end
 
 @implementation SelectMapViewController
 #pragma mark 初始化
 
+- (NSMutableArray *)alternativeAddressesM
+{
+	if(_alternativeAddressesM == nil)
+	{
+		_alternativeAddressesM = [NSMutableArray array];
+	}
+	return _alternativeAddressesM;
+}
+- (NSMutableArray *)historyAddressM
+{
+	if(_historyAddressM == nil)
+	{
+		_historyAddressM = [NSMutableArray array];
+	}
+	return _historyAddressM;
+}
+
+- (CLGeocoder *)geocoder
+{
+	if (!_geocoder) {
+		self.geocoder = [[CLGeocoder alloc] init];
+	}
+	return _geocoder;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     
@@ -41,6 +69,11 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)dealloc
+{
+	[[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 - (void)setupUI
@@ -68,7 +101,7 @@
     self.searchBar.keyboardType       = UIKeyboardTypeDefault;
     self.searchBar.autocorrectionType = UITextAutocorrectionTypeNo;
     self.searchBar.autocapitalizationType = UITextAutocapitalizationTypeNone;
-    
+	
     [self.contentView addSubview: self.searchBar];
 	
     self.searchHistoryTableView.frame           = CGRectMake(0, CGRectGetMaxY(self.searchBar.frame), MainWidth, MainHeight - self.searchBar.frame.size.height);
@@ -90,7 +123,10 @@
 
 - (void)setupData
 {
-    
+    //假地址
+	for (int i =0; i<30; i++) {
+		[self.historyAddressM addObject:[NSString stringWithFormat:@"历史地址%d",i]];
+	}
 }
 
 - (void)keyBoardWillShow:(NSNotification *)note
@@ -144,12 +180,37 @@
 {
     
     [searchBar resignFirstResponder];
+	if(self.searchBar.text.length > 0)
+	{
+		__weak __typeof(self)weakSelf = self;
+		dispatch_queue_t concurrentQueue = dispatch_queue_create("getGeocodeWithAddress.concurrent.queue", DISPATCH_QUEUE_CONCURRENT);
+		dispatch_async(concurrentQueue, ^(){
+			[weakSelf getGeocodeWithAddress:weakSelf.searchBar.text];
+		});
+		
+	}
 }
 
 //开始编辑
 - (void)searchBarTextDidBeginEditing:(UISearchBar *)searchBar
 {
     
+}
+
+//文字改变
+- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText
+{
+	if(searchText.length > 0)
+	{
+		[self.alternativeAddressesM removeAllObjects];
+		[self.searchHistoryTableView reloadData];
+		__weak __typeof(self)weakSelf = self;
+		dispatch_queue_t concurrentQueue = dispatch_queue_create("getGeocodeWithAddress.concurrent.queue", DISPATCH_QUEUE_CONCURRENT);
+		dispatch_async(concurrentQueue, ^(){
+			[weakSelf getGeocodeWithAddress:weakSelf.searchBar.text];
+		});
+		
+	}
 }
 
 #pragma mark tableView代理
@@ -171,8 +232,11 @@
 		headerLabelX = 20.0;
 	}
 	headerLabel.frame                = CGRectMake(headerLabelX, 0.0, MainWidth, 44.0);
-    headerLabel.text                 = @"搜索历史";
-	
+	if(section == 0){
+		headerLabel.text             = @"您可能在找";
+	}else{
+		headerLabel.text             = @"搜索历史";
+	}
 	[customView addSubview:headerLabel];
 	
 	return customView;
@@ -182,9 +246,18 @@
 	return 44.0;
 }
 
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+	return 2;
+}
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 30;
+	if(section == 0){
+		return self.alternativeAddressesM.count;
+	}else{
+		return self.historyAddressM.count;
+	}
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -194,10 +267,22 @@
     if (!cell) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellId];
     }
-    cell.textLabel.text      = [NSString stringWithFormat:@"测试数据%ld",indexPath.row];
-    cell.textLabel.textColor = [UIColor whiteColor];
+	cell.textLabel.textColor = [UIColor whiteColor];
     cell.backgroundColor     = [UIColor clearColor];
     cell.selectionStyle      = UITableViewCellSelectionStyleNone;
+	
+	if(indexPath.section == 0)
+	{
+		//推荐地址
+		if(self.alternativeAddressesM.count >0){
+			cell.textLabel.text		 = [(CLPlacemark *)self.alternativeAddressesM[indexPath.row] name];
+		}
+	}else{
+		//历史地址
+		if(self.historyAddressM.count >0){
+			cell.textLabel.text      = self.historyAddressM[indexPath.row];
+		}
+	}
 	
     return cell;
 }
@@ -234,10 +319,10 @@
 {
     userLocation.title    = @"您的位置";
     userLocation.subtitle = @"您当前所在位置";
-    
-    CLLocationCoordinate2D center = userLocation.location.coordinate;
-    FZ_LOG(@"%f %f", center.latitude, center.longitude);
-    
+	self.userLocation = userLocation;
+//    CLLocationCoordinate2D center = userLocation.location.coordinate;
+//    FZ_LOG(@"%f %f", center.latitude, center.longitude);
+	
     // 设置地图的中心点（以用户所在的位置为中心点）
 //    [mapView setCenterCoordinate:userLocation.location.coordinate animated:YES];
     
@@ -246,6 +331,133 @@
 //    MKCoordinateRegion region = MKCoordinateRegionMake(center, span);
 //    [mapView setRegion:region animated:YES];
 }
+
+/**
+ *  地理编码
+ */
+- (void)getGeocodeWithAddress:(NSString *)address {
+	if (address.length == 0) return;
+	
+	__weak __typeof(self)weakSelf = self;
+	[self.geocoder geocodeAddressString:address completionHandler:^(NSArray *placemarks, NSError *error) {
+		if (error) {
+			// 有错误（地址乱输入）
+			FZ_LOG(@"没有找到这个地址");
+		} else {
+			NSMutableArray *distanceArrayM = [NSMutableArray array];
+			for (CLPlacemark *pm in placemarks) {
+				[weakSelf.alternativeAddressesM addObject:pm];
+				double lat = pm.location.coordinate.latitude;
+				double lon = pm.location.coordinate.longitude;
+				double distance = [weakSelf distanceBetweenOrderBylat1:lat lat2:self.userLocation.coordinate.latitude lon1:lon lon2:self.userLocation.coordinate.longitude];
+				[distanceArrayM addObject:[NSNumber numberWithDouble:distance]];
+			}
+			//排序
+			for(int i=0; i<distanceArrayM.count; i++)
+			{
+				for(int j =i; j<distanceArrayM.count; j++)
+				{
+					if ([distanceArrayM[i] doubleValue] > [distanceArrayM[j] doubleValue]) {
+						[distanceArrayM exchangeObjectAtIndex:i withObjectAtIndex:j];
+						[self.alternativeAddressesM exchangeObjectAtIndex:i withObjectAtIndex:j];
+					}
+				}
+			}
+			
+			for (NSNumber *distance in distanceArrayM) {
+				FZ_LOG(@"%@",distance);
+			}
+			//加载完数据后 刷新表格
+			dispatch_async(dispatch_get_main_queue(), ^{
+				[weakSelf.searchHistoryTableView reloadData];
+			});
+			
+			// 编码成功
+			// 取出最前面的地址
+			CLPlacemark *pm = [placemarks firstObject];
+			
+			// 设置经纬度
+			FZ_LOG(@"%@",[NSString stringWithFormat:@"%f", pm.location.coordinate.latitude]);
+			FZ_LOG(@"%@",[NSString stringWithFormat:@"%f", pm.location.coordinate.longitude]);
+			
+			// 设置具体地址
+			FZ_LOG(@"%@",pm.name);
+			
+			NSLog(@"总共找到%ld个地址", placemarks.count);
+
+			for (CLPlacemark *pm in placemarks) {
+				NSLog(@"-----地址开始----");
+
+				NSLog(@"%f %f %@", pm.location.coordinate.latitude, pm.location.coordinate.longitude, pm.name);
+
+				[pm.addressDictionary enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+					NSLog(@"%@ %@", key, obj);
+				}];
+
+				NSLog(@"-----地址结束----");
+			}
+			
+			
+		}
+	}];
+	
+	
+}
+
+/**
+ *  获取两点经纬度地址的方法 返回的单位是公里
+ *  @return A，B连点的距离
+ */
+-(double)distanceBetweenOrderBylat1:(double)lat1 lat2:(double)lat2 lon1:(double)lng1 lon2:(double)lng2{
+	double dd = M_PI/180;
+	double x1=lat1*dd,x2=lat2*dd;
+	double y1=lng1*dd,y2=lng2*dd;
+	double R = 6371004;
+	double distance = (2*R*asin(sqrt(2-2*cos(x1)*cos(x2)*cos(y1-y2) - 2*sin(x1)*sin(x2))/2));
+	//km  返回
+	//     return  distance*1000;
+	
+	//返回 公里
+	return   distance/1000;
+ 
+}
+
+/**
+ *  反地理编码
+ */
+//- (IBAction)getAddressWithGeocode: {
+//	// 1.包装位置
+//	CLLocationDegrees latitude = [self.latitudeField.text doubleValue];
+//	CLLocationDegrees longitude = [self.longtitudeField.text doubleValue];
+//	CLLocation *loc = [[CLLocation alloc] initWithLatitude:latitude longitude:longitude];
+//	
+//	// 2.反地理编码
+//	[self.geocoder reverseGeocodeLocation:loc completionHandler:^(NSArray *placemarks, NSError *error) {
+//		if (error) { // 有错误（地址乱输入）
+//			self.reverseDetailAddressLabel.text = @"你找的地址可能只在火星有！！！";
+//		} else { // 编码成功
+//			// 取出最前面的地址
+//			CLPlacemark *pm = [placemarks firstObject];
+//			
+//			// 设置具体地址
+//			self.reverseDetailAddressLabel.text = pm.name;
+//			//            NSLog(@"总共找到%d个地址", placemarks.count);
+//			//
+//			//            for (CLPlacemark *pm in placemarks) {
+//			//                NSLog(@"-----地址开始----");
+//			//
+//			//                NSLog(@"%f %f %@", pm.location.coordinate.latitude, pm.location.coordinate.longitude, pm.name);
+//			//
+//			//                [pm.addressDictionary enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+//			//                    NSLog(@"%@ %@", key, obj);
+//			//                }];
+//			//
+//			//                NSLog(@"-----地址结束----");
+//			//            }
+//		}
+//	}];
+//}
+
 
 //回到用户位置
 - (void)backToUserLocation {
